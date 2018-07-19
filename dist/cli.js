d8c3a702cbe12868fb1b727002cd2e37289636f7
Brendan Ward <bcward@consbio.org>
2018-07-19 16:51:40 -0700

    Added bounds to CLI

--- dist/cli.js
+++ dist/cli.js
@@ -30,53 +30,64 @@ var parseListToFloat = function parseListToFloat(text) {
     return text.split(',').map(Number);
 };
 
-_commander2.default.version(_package.version).description('Export a Mapbox GL map to image').arguments('<style.json> <img_filename> <width> <height>').option('-c, --center <longitude,latitude>', 'center of map (NO SPACES)', parseListToFloat).option('-z, --zoom <n>', 'Zoom level', parseInt).parse(process.argv);
-
-// console.log(program)
+_commander2.default.version(_package.version).description('Export a Mapbox GL map to image.  You must provide either center and zoom, or bounds.').arguments('<style.json> <img_filename> <width> <height>').option('-c, --center <longitude,latitude>', 'center of map (NO SPACES)', parseListToFloat).option('-z, --zoom <n>', 'Zoom level', parseInt).option('-b, --bounds <west,south,east,north>', 'Bounds (NO SPACES)', parseListToFloat).parse(process.argv);
 
 var _cli$args = _slicedToArray(_commander2.default.args, 4),
     styleFilename = _cli$args[0],
     imgFilename = _cli$args[1],
     width = _cli$args[2],
     height = _cli$args[3],
-    center = _commander2.default.center,
-    zoom = _commander2.default.zoom;
+    _cli$center = _commander2.default.center,
+    center = _cli$center === undefined ? null : _cli$center,
+    _cli$zoom = _commander2.default.zoom,
+    zoom = _cli$zoom === undefined ? null : _cli$zoom,
+    _cli$bounds = _commander2.default.bounds,
+    bounds = _cli$bounds === undefined ? null : _cli$bounds;
 
 var imgWidth = parseInt(width, 10);
 var imgHeight = parseInt(height, 10);
-
-// console.log('args: %j %j %j %j', style, imgFilename, width, height)
 
 if (!_fs2.default.existsSync(styleFilename)) {
     raiseError('Style JSON file does not exist: ' + styleFilename);
 }
 
 if (imgWidth <= 0 || imgHeight <= 0) {
-    raiseError('Width and height must be greater than 0, they are width:' + imgWidth + ' height:' + Height);
+    raiseError('Width and height must be greater than 0, they are width:' + imgWidth + ' height:' + imgHeight);
 }
 
-if (center.length !== 2) {
-    raiseError('Center must be longitude,latitude.  Invalid value found: ' + [].concat(_toConsumableArray(center)));
+if (center !== null) {
+    if (center.length !== 2) {
+        raiseError('Center must be longitude,latitude.  Invalid value found: ' + [].concat(_toConsumableArray(center)));
+    }
+
+    if (Math.abs(center[0]) > 180) {
+        raiseError('Center longitude is outside world bounds (-180 to 180 deg): ' + center[0]);
+    }
+
+    if (Math.abs(center[1]) > 90) {
+        raiseError('Center latitude is outside world bounds (-90 to 90 deg): ' + center[1]);
+    }
 }
 
-if (Math.abs(center[0]) > 180) {
-    raiseError('Center longitude is outside world bounds (-180 to 180 deg): ' + center[0]);
-}
-
-if (Math.abs(center[1]) > 90) {
-    raiseError('Center latitude is outside world bounds (-90 to 90 deg): ' + center[1]);
-}
-
-if (zoom < 0 || zoom > 22) {
+if (zoom !== null && (zoom < 0 || zoom > 22)) {
     raiseError('Zoom level is outside supported range (0-22): ' + zoom);
+}
+
+if (bounds !== null) {
+    if (bounds.length !== 4) {
+        raiseError('Bounds must be west,south,east,north.  Invalid value found: ' + [].concat(_toConsumableArray(bounds)));
+    }
 }
 
 console.log('\n\n-------- Export Mapbox GL Map --------');
 console.log('style: %j', styleFilename);
 console.log('output image (' + width + 'w x ' + height + 'h) to: ' + imgFilename + '\n\n');
+// console.log('center: %j', center)
+// console.log('zoom: %j', zoom)
+// console.log('bounds: %j', bounds)
 
 var style = JSON.parse(_fs2.default.readFileSync(styleFilename));
 
-(0, _render2.default)(style, imgWidth, imgHeight, { zoom: zoom, center: center }).then(function (data) {
+(0, _render2.default)(style, imgWidth, imgHeight, { zoom: zoom, center: center, bounds: bounds }).then(function (data) {
     _fs2.default.writeFileSync(imgFilename, data);
 });
